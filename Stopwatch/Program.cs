0662ab7bac134f4b1d9310e5376b3a38ec7a198a
Luciana Santos <luciydss@hotmail.com>
2023-05-19 10:34:09 -0300

    Stopwatch project added

---
+++ Stopwatch/Program.cs
@@ -0,0 +1,68 @@
+﻿using System.Threading;
+using static System.Console;
+
+namespace Stopwatch
+{
+    class Program
+    {
+        static void Main()
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Clear();
+            WriteLine("S = Segundos => <<10s>> = 10 segundos");
+            WriteLine("M = Minuto => <<1m>> = 1 minutos");
+            WriteLine("0 = Sair");
+            WriteLine("Quanto tempo deseja contar?");
+
+            string data = ReadLine().ToLower();
+            char type = char.Parse(data.Substring(data.Length - 1, 1));
+            int time = int.Parse(data.Substring(0, data.Length - 1));
+            WriteLine(time);
+            WriteLine(type);
+
+            int multiplier = 1;
+
+            if (type == 'm')
+                multiplier = 60;
+
+            if (time == 0)
+                System.Environment.Exit(0);
+
+            PreStart(time * multiplier);
+        }
+
+        static void PreStart(int time)
+        {
+            Clear();
+            WriteLine("Ready...");
+            Thread.Sleep(1000);
+            WriteLine("Set...");
+            Thread.Sleep(1000);
+            WriteLine("Go...");
+            Thread.Sleep(2500);
+
+            Start(time);
+        }
+
+        static void Start(int time)
+        {
+            int currentTime = 0;
+
+            while (currentTime != time)
+            {
+                Clear();
+                currentTime++;
+                WriteLine(currentTime);
+                Thread.Sleep(1000);
+            }
+
+            Clear();
+            WriteLine("Stopwatch finalizado.");
+            Thread.Sleep(2500);
+        }
+    }
+}
